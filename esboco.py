c01176451be036d2e09f855dbd34869661efd5af
samuelkenji <sa.kgs10@gmail.com>
2018-05-22 13:53:17 -0300

    Initial commit

---
+++ esboco.py
@@ -0,0 +1,93 @@
+from time import time
+import random
+import operator
+
+pontuacao=1000
+
+lista=[]
+
+operacoes=[
+    ('+', operator.add),
+    ('-', operator.sub),
+    ('*', operator.mul),
+    ('/', operator.floordiv),
+]
+
+def questao_facil():
+    global lista
+    op_op, op_res=random.choice(operacoes)
+    n1=random.randint(1,20)
+    n2=random.randint(1,20)
+    if n1>n2:
+        quest = '{}{}{}'.format(n1, op_op, n2)
+        res=op_res(n1,n2)
+    else:
+        quest = '{}{}{}'.format(n2,op_op,n1)
+        res=op_res(n2,n1)
+    lista.insert(0,quest)
+    lista.insert(1,res)
+
+
+def questao_media():
+    op_op, op_res = random.choice()
+    n1 = random.randint(1, 100)
+    n2 = random.randint(1, 100)
+    quest = '{}{}{}'.format(n1, op_op, n2)
+    res = op_res(n1, n2)
+    return quest, res
+
+def questao_dificil():
+    op_op, op_res = random.choice()
+    n1 = random.randint(1, 250)
+    n2 = random.randint(1, 250)
+    quest = '{}{}{}'.format(n1, op_op, n2)
+    res = op_res(n1, n2)
+    print (res)
+    return quest, res
+
+def quiz(pontuacao):
+    print('Quanto é {}'.format(lista[0]))
+    t0=time()
+    res_jogador=int(input('Digite a resposta: '))
+    tf=time()-t0
+    pontuacao_total=0+(pontuacao-tf)
+    if lista[1]==res_jogador:
+        print('Resposta certa!')
+    else:
+        print('Resposta errada')
+    return pontuacao_total
+
+def facil():
+    for _ in range(1):
+        questao_facil()
+        quiz(pontuacao)
+        print(lista[0])
+        print(int(pontuacao_total))
+
+def medio():
+    for _ in range(2):
+        questao_media()
+        quiz(quest, res, pontuacao_total)
+
+
+def dificil():
+    for _ in range(3):
+        questao_dificil()
+        quiz(quest, res, pontuacao_total)
+
+def menu():
+    print('Bem-vindo ao jooj')
+    dificuldade=int(input('Selecione a dificuldade:\n1 - Fácil\n2 - Médio\n3 - Difícil'))
+    while dificuldade<1 or dificuldade>3:
+        dificuldade = int(input('Selecione a dificuldade:\n1 - Fácil\n2 - Médio\n3 - Difícil'))
+    if dificuldade==1:
+        facil()
+    elif dificuldade==2:
+        medio()
+    elif dificuldade==3:
+        dificil()
+
+def main():
+    menu()
+
+main()
